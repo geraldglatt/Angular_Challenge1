7a846b41b6c38989aa547cfec263586c56c921fd
geraldglatt <geraldglatt89@gmail.com>
2022-03-22 13:36:13 +0100

    Change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular_Challenge1';
+  title = 'Bienvenue sur le site de Gerald Glatt !';
 }
