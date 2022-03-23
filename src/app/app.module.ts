bd253917052a4fae56205e1dab1b99ac1959f51a
geraldglatt <geraldglatt89@gmail.com>
2022-03-23 13:39:39 +0100

    Challenge2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ValidationButtonComponent } from './validation-button/validation-button.component';
+import { BlockComponent } from './block/block.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ValidationButtonComponent,
+    BlockComponent
   ],
   imports: [
     BrowserModule
